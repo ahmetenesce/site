a2c1a4e757c1fe44d2655cd4d906f27e34fed0f0
ahmetenesce <ahmetenesturkiu@gmail.com>
2019-02-16 22:08:30 +0300

    Updates

---
+++ precache-manifest.ac44d6f2800fb71073332b0d21578714.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4b54bfd49c02f9eaead",
+    "url": "/site/static/js/runtime~main.c4b54bfd.js"
+  },
+  {
+    "revision": "9446df5f5a836f0efc05",
+    "url": "/site/static/js/main.9446df5f.chunk.js"
+  },
+  {
+    "revision": "fd965b058396d394f040",
+    "url": "/site/static/js/2.fd965b05.chunk.js"
+  },
+  {
+    "revision": "9446df5f5a836f0efc05",
+    "url": "/site/static/css/main.bea7be45.chunk.css"
+  },
+  {
+    "revision": "127c15929d431e6fa0cc17667553a74c",
+    "url": "/site/index.html"
+  }
+];
